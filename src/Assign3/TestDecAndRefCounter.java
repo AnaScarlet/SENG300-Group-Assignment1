bb1e86fb0428fe23a29b2a679266c2ad5be3fc1d
Anastasiya Lazarenko <laz1anastasiya@gmail.com>
2018-03-08 16:04:06 -0700

    Get rid of /temp, add jars, change main()

---
+++ src/Assign3/TestDecAndRefCounter.java
@@ -0,0 +1,120 @@
+	/**
+	 *  @authors			    Anastasiya Lazarenko, Matthew Buhler, Zachary Hull 
+	 */
+
+
+
+
+
+package src.Assign3;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestDecAndRefCounter {
+//	
+//    /**
+//     * Checks if the program will compute the correct number of
+//     * declarations and references using "Class" as the type
+//     * to search for.
+//     */
+//    @Test
+//    public void test_class_declaration() {
+//    	int total = new Main("path name", "java.lang.Class");
+//    	assertEquals(expected, total);
+//    }
+//    
+//	/**
+//     * Checks if the program will compute the correct number of
+//     * declarations and references using "Enumerator" as the type
+//     * to search for. 
+//     */
+//    @Test
+//    public void test_Enum_declaration() {
+//    	int total = new Main("path name", "java.lang.Enumerator");
+//    	assertEquals(expected, total);
+//    }
+//    
+//    /**
+//     * Checks if the program will compute the correct number of
+//     * declarations and references using "Interface" as the type 
+//     * to search for.
+//     */
+//    @Test
+//    public void test_Interface_Declaration() {
+//    	int total = new Main("path name", "java.lang.Interface");
+//    	assertEquals(expected, total);
+//    }
+//    
+//    /**
+//     * Checks if the program will compute the correct number of
+//     * declarations and references using "Annotation" as the type
+//     * to search for.
+//     */
+//    @Test
+//    public void test_Annotation_Declaration() {
+//    	int total = new Main("path name", "java.lang.Annotation");
+//    	assertEquals(expected, total);
+//    }
+//    
+//    /**
+//     * Checks if the program will throw InvalidArgumentException
+//     * for an invalid path but a valid declaration type
+//     */
+//    @Test(expected = IllegalArgumentException.class)
+//    public void test_Invalid_Path_Valid_Dec() {
+//    	int total = new Main("path name", "java.lang.Class");
+//    }
+//    
+//    /**
+//     * Checks if the program will throw InvalidArgumentException 
+//     * for an invalid declaration type but a valid path
+//     */
+//    @Test(expected = IllegalArgumentException.class)
+//    public void test_Invalid_Dec_Valid_Path() {
+//    	int total = new Main("path name", "java.lang.Class");
+//    }
+//    
+//    /**
+//     * Checks if the program will throw InvalidArgumentException
+//     * when both the path and declaration type are invalid
+//     */
+//    @Test(expected = IllegalArgumentException.class)
+//    public void test_Ivalid_Path_and_Dec() {
+//    	int total = new Main("path name", "java.lang.Class");
+//    }
+//    
+//	/**
+//     * Checks if the program will return 0 when the directory has
+//     * no declarations or references of "Enumerator" as the type
+//     * to search for.
+//     */
+//    @Test
+//    public void test_Enum_declaration_With_None() {
+//    	int total = new Main("path name", "java.lang.Enumerator");
+//    	assertEquals( 0, total);
+//    }
+//    
+//    /**
+//     * Checks if the program will return 0 when the directory has
+//     * no declarations or references of "Interface" as the type
+//     * to search for.
+//     */
+//    @Test
+//    public void test_Interface_Declaration_With_None() {
+//    	int total = new Main("path name", "java.lang.Interface");
+//    	assertEquals( 0, total);
+//    }
+//    
+//    /**
+//     * Checks if the program will return 0 when the directory has
+//     * no declarations or references of "Annotation" as the type
+//     * to search for.
+//     */
+//    @Test
+//    public void test_Annotation_Declaration_With_None() {
+//    	int total = new Main("path name", "java.lang.Annotation");
+//    	assertEquals( 0, total);
+//    }
+}
+
