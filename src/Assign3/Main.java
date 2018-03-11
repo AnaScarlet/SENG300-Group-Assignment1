4359a2e45bca1ea2471cc0e301079923174191e5
DESKTOP-0O5R0K9\MattB <matthewmbuhler@gmail.com>
2018-03-11 11:44:16 -0600

    Updated MyParser and Main to look through references and declararations. You need to pass it a source directory and one of type Class, Enum, Annotation or Interface.
    Works for Class, need to test Enum, Annocation and Interfaces. Likely will fail for those.

--- src/Assign3/Main.java
+++ src/Assign3/Main.java
@@ -1,11 +1,11 @@
 /**
- * @authors			    Anastasiya Lazarenko, Matthew Buhler, Zachary Hull 
+ * @authors			    Anastasiya Lazarenko, Matthew Buhler, Zachary Hull
  * @team                1
  * @version             Group Assignment 1
  * @since               March 14th, 2018
  *
  * Course:              SENG300, University of Calgary
- * Instructor:          Prof. Robert Walker 
+ * Instructor:          Prof. Robert Walker
  *
  * 1. takes a pathname to indicate a directory of interest,
  * 2. takes a string to indicate a fully qualified name of a Java type,
@@ -23,7 +23,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
- 
+
 import org.eclipse.jdt.core.dom.AST;
 import org.eclipse.jdt.core.dom.ASTNode;
 import org.eclipse.jdt.core.dom.ASTParser;
@@ -31,10 +31,10 @@ import org.eclipse.jdt.core.dom.ASTVisitor;
 import org.eclipse.jdt.core.dom.CompilationUnit;
 import org.eclipse.jdt.core.dom.SimpleName;
 import org.eclipse.jdt.core.dom.VariableDeclarationFragment;
- 
+
 
 public class Main {
-	
+
 	/* TODO: Looking for references to classes
 		String s = (String) new Integer(); - CastExpression, ClassInstanceCreation
 		String.CASE_INSENSITIVE_ORDER;
@@ -46,15 +46,15 @@ public class Main {
 		try {
 			path = args[0];
 			typeDec = args[1];
-			
+
 		} catch (java.lang.ArrayIndexOutOfBoundsException e) {
 			System.out.println("You did not provide enough arguments!");
 			System.out.println("Please try again ");
-			Scanner reader = new Scanner(System.in);  
+			Scanner reader = new Scanner(System.in);
 			System.out.println("Enter a path: ");
-			path = reader.next(); 
+			path = reader.next();
 			System.out.println("Enter a Java type declaration: ");
-			typeDec = reader.next(); 
+			typeDec = reader.next();
 			reader.close();
 			if (typeDec.equals(""))
 				throw new InvalidArgumentsException();
@@ -66,6 +66,8 @@ public class Main {
 		} catch (IOException e) {
 			System.out.println("An exception occurred while parsing the given directory.");
 		}
-		// TODO: get counts of references and of declarations from parser and return them to user
-	} 
+    //System.out.println("For your selection of " + typeDec +":");
+    System.out.println("Number of References: " + parser.getReferences());
+    System.out.println("Number of Declarations: " + parser.getDeclarations());
+	}
 }
